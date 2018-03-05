7db1d5b1708e9c15ad2d068598207cdf70cc2269
Phoebe Lau <phoebelau7@gmail.com>
2018-03-05 22:12:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 // Write your code in this file!
 const currentUser = 'Grace Hopper';
-let welcomeMessage = "Welcome to Flatbook, "currentUser
+let welcomeMessage = "Welcome to Flatbook, "
+welcomeMessage currentUser
